e6614f6389f28d80bdc6a223b8f3c56f8506259c
Ronak Goyal <ronakgoyal1403@gmail.com>
2024-06-24 11:26:29 +0530

    getter setter in class

---
+++ 08_oops/10_getterSetter.js
@@ -0,0 +1,75 @@
+// getter setter in class (best)
+
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "abc")
+console.log(hitesh.email);
+
+// getter setter in function 
+
+// function User(email, password){
+//     this._email = email;
+//     this._password = password
+
+//     Object.defineProperty(this, 'email', {
+//         get: function(){
+//             return this._email.toUpperCase()
+//         },
+//         set: function(value){
+//             this._email = value
+//         }
+//     })
+//     Object.defineProperty(this, 'password', {
+//         get: function(){
+//             return this._password.toUpperCase()
+//         },
+//         set: function(value){
+//             this._password = value
+//         }
+//     })
+
+// }
+
+// const chai = new User("chai@chai.com", "chai")
+
+// console.log(chai.email);
+
+
+// getter setter in objects
+
+// const User = {
+//     _email: 'h@hc.com',
+//     _password: "abc",
+
+
+//     get email(){
+//         return this._email.toUpperCase()
+//     },
+
+//     set email(value){
+//         this._email = value
+//     }
+// }
+
+// const tea = Object.create(User)
+// console.log(tea.email);
